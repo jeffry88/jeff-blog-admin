9041f214d2c263368113aeaec20723949c435dfe
dengdianghua <724993441@qq.com>
2019-09-19 18:29:24 +0800

    新增自定义hash 形式前端路由，实现单页应用并按需加载js 文件

---
+++ static/js/add-article.js
@@ -0,0 +1,26 @@
+//新建文章
+function add_article() {
+    // $(".container-fluid").html("新建文章");
+    var title_html = '';
+    title_html +=
+    '<div class="layui-form-item title-head">'+
+    '    <label class="layui-form-label title-label">标题</label>'+
+    '    <div class="layui-input-block">'+
+    '        <input type="text" name="title" required  lay-verify="required" placeholder="请输入文章标题" autocomplete="off" class="layui-input title-input">'+
+    '        <button type="button" class="layui-btn jeff-button">发布</button>'+
+    '    </div>'+
+    '</div>'+
+    '<div class="add-article-body" id="add_article_body"></div>';
+    $(".container-fluid").html(title_html);
+
+
+    var article_add_editor = editormd("add_article_body", {
+        width: "100%",
+        height:"90%",
+        // autoHeight : true,
+        markdown: "新建文章",     // dynamic set Markdown text
+        path: "../static/vendor/editor.md/lib/" , // Autoload modules mode, codemirror, marked... dependents libs path,
+        imageUpload : true,
+        imageFormats : [ "jpg", "jpeg", "gif", "png", "bmp", "webp" ]
+    });
+}
